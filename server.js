c8474438b6f6a9c236043578062c12cd90b9d057
Grant Rudow <webdev.grant@gmail.com>
2020-09-23 21:54:42 -0500

    working on attaching aws to server

--- server.js
+++ server.js
@@ -2,12 +2,34 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const pg = require('pg');
 
 const PORT = 4000;
+
+const connection = pg.createConnection({
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log('Database connection failed: ' + err.stack);
+        return;
+    }
+    console.log('Conneted database')
+})
 
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.json('This is the home page')
+})
+
 app.listen(PORT, () => {
     console.log('WE OUT HERE BOIIII')
 })
+
+connection.end();
